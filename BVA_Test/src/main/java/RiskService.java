56366b7d224bc62e0240b6b63f965b5986f83f0a
Huybodoithe <pnhuy1904@gmail.com>
2026-09-22 22:23:16 +0700

    Inject bugs into the code and change test suit

--- BVA_Test/src/main/java/RiskService.java
+++ BVA_Test/src/main/java/RiskService.java
@@ -1,17 +1,19 @@
 public class RiskService {
-    public static String evaluateRisk(int creditScore, int term) {
+    public static String evaluateRisk(int credit_score, int term) {
         // Kiểm tra miền giá trị hợp lệ toàn cục
-        if (creditScore < 300 || creditScore > 850 || term < 6 || term > 60) {
+        // Cấy lỗi creditScore sai miền bị Invalid
+        if (credit_score <= 300 || credit_score >= 850 || term < 6 || term > 60) {
             return "Invalid";
         }
 
         // Điểm tín dụng thấp: 300 <= credit_score < 580
-        if (creditScore < 580) {
+        if (credit_score < 580) {
             return "High Risk";
         }
 
         // Điểm tín dụng trung bình: 580 <= credit_score < 700
-        if (creditScore < 700) {
+        // Cấy lỗi đánh nhầm 700 thành 800
+        if (credit_score < 800) {
             if (term <= 24) { // 6 <= term <= 24: kỳ hạn ngắn
                 return "Low Risk";
             } else {          // 25 <= term <= 60: kỳ hạn dài
--- BVA_Test/src/main/java/ScholarshipService.java
+++ BVA_Test/src/main/java/ScholarshipService.java
@@ -8,15 +8,17 @@ public class ScholarshipService {
             return "Invalid";
         }
 
-        // 2. Học bổng Xuất sắc: GPA >= 3.6 và ĐRL >= 90
+        // 2. Học bổng Giỏi: GPA >= 3.2 và ĐRL >= 80
+        if (roundedGpa >= 3.2 && drl >= 80) {
+            return "Gioi";
+        }
+
+        // 3. Học bổng Xuất sắc: GPA >= 3.6 và ĐRL >= 90
         if (roundedGpa >= 3.6 && drl >= 90) {
             return "Xuat Sac";
         }
 
-        // 3. Học bổng Giỏi: GPA >= 3.2 và ĐRL >= 80 (các trường hợp Xuất sắc đã được return ở trên)
-        if (roundedGpa >= 3.2 && drl >= 80) {
-            return "Gioi";
-        }
+
 
         // 4. Các trường hợp hợp lệ còn lại không đủ điều kiện nhận học bổng
         return "Khong du dieu kien";
