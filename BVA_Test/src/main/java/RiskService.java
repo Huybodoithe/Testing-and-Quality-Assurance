23601fd4d98775eb2357cae7cbe1b19e2bc509a3
Huybodoithe <pnhuy1904@gmail.com>
2026-09-22 22:27:24 +0700

    Fix all bugs

--- BVA_Test/src/main/java/RiskService.java
+++ BVA_Test/src/main/java/RiskService.java
@@ -1,8 +1,7 @@
 public class RiskService {
     public static String evaluateRisk(int credit_score, int term) {
         // Kiểm tra miền giá trị hợp lệ toàn cục
-        // Cấy lỗi creditScore sai miền bị Invalid
-        if (credit_score <= 300 || credit_score >= 850 || term < 6 || term > 60) {
+        if (credit_score < 300 || credit_score > 850 || term < 6 || term > 60) {
             return "Invalid";
         }
 
@@ -12,8 +11,7 @@ public class RiskService {
         }
 
         // Điểm tín dụng trung bình: 580 <= credit_score < 700
-        // Cấy lỗi đánh nhầm 700 thành 800
-        if (credit_score < 800) {
+        if (credit_score < 700) {
             if (term <= 24) { // 6 <= term <= 24: kỳ hạn ngắn
                 return "Low Risk";
             } else {          // 25 <= term <= 60: kỳ hạn dài
